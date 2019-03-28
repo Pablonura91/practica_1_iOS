eb7d625f6b9fc6c4f1de708360300638d140fc34
Pol Vañó <pol98vano@gmail.com>
2019-03-28 15:59:14 +0100

    create pelicula

---
+++ Practica_UF1/Pelicula.swift
@@ -0,0 +1,17 @@
+//
+//  Pelicula.swift
+//  Practica_UF1
+//
+//  Created by Alumne on 28/03/2019.
+//  Copyright © 2019 LaSalle. All rights reserved.
+//
+
+import Foundation
+
+struct Pelicula {
+    var image:String
+    var title:String
+    var horario:String
+    var sinopsis:String
+}
+
