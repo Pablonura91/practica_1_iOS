4434a0032417ca24ccf04a193c38f2ebd970a92a
Pol Vañó <pol98vano@gmail.com>
2019-03-28 16:59:37 +0100

    los commits de xcode no funcan

---
+++ Practica_UF1/DetailFilmViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailFilmViewController.swift
+//  Practica_UF1
+//
+//  Created by Alumne on 28/03/2019.
+//  Copyright © 2019 LaSalle. All rights reserved.
+//
+
+import UIKit
+
+class DetailFilmViewController: UIViewController {
+
+    var pelicula: Pelicula?
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var detailTitle: UILabel!
+    @IBOutlet weak var horario: UILabel!
+    @IBOutlet weak var sinopsis: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let currentPelicula = pelicula{
+            self.pelicula = currentPelicula
+            
+            image.image = UIImage(named: currentPelicula.image)
+            detailTitle.text = currentPelicula.title
+            horario.text = currentPelicula.horario
+            sinopsis.text = currentPelicula.sinopsis            
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "saveDetailFilm" {
+            if let rvc = segue.destination as? PeliculaTableViewController{
+                rvc.pelicula = self.pelicula
+            }
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Practica_UF1/PeliculaManager.swift
@@ -0,0 +1,23 @@
+//
+//  PeliculaManager.swift
+//  Practica_UF1
+//
+//  Created by Alumne on 28/03/2019.
+//  Copyright © 2019 LaSalle. All rights reserved.
+//
+
+import Foundation
+
+class PeliculaManager {
+    
+    var peliculas:[Pelicula]=[Pelicula]()
+    
+    func addPelicula(pelicula: Pelicula){
+        self.peliculas.append(pelicula)
+    }    
+    
+    func deletePelicula(index: Int){
+        self.peliculas.remove(at: index)
+    }
+    
+}
