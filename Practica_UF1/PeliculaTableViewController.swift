004d5aaf1e6853525360dc16444610374fc8cb9a
Pol Vañó <pol98vano@gmail.com>
2019-04-09 20:12:16 +0200

    documentacion

--- Practica_UF1/PeliculaTableViewController.swift
+++ Practica_UF1/PeliculaTableViewController.swift
@@ -19,14 +19,15 @@ class PeliculaTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.dataBaseManager = DatabaseManager()
-        //peliculasManager = dataBaseManager.readRecords(database) as? PeliculaManager
-        
+        self.dataBaseManager = DatabaseManager()
+        peliculasManager = dataBaseManager.readRecords(database) as? PeliculaManager
+        tableView.reloadData()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
